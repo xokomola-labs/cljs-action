696194bb2de7e4423d4b535670c0c00f67862b05
Marc van Grootel <marc.van.grootel@gmail.com>
2022-10-29 20:21:23 +0200

    Add Saxon-JS

--- action.cljs
+++ action.cljs
@@ -1,6 +1,32 @@
 (ns action
   (:require ["@actions/core" :as core]
-            ["@actions/github" :as github]))
+            ["@actions/github" :as github]
+            ["saxon-js" :as xslt]))
+
+(defn node-slurp
+  "Takes a file path string and reads it's contents as UTF-8 text."
+  [path]
+  (let [fs (js/require "fs")]
+    (.readFileSync fs path "utf8")))
+
+(defn node-write
+  "Takes a path string and a contents string and writes it to the 
+   filesystem."
+  [path contents]
+  (let [fs (js/require "fs")]
+    (.writeFileSync fs path contents)))
+
+(def xf
+  (.. xslt -default -transform))
+
+(defn transform
+  [source]
+  (let [options {:stylesheetFileName "source.sef.json"
+                 :sourceText source
+                 :sourceType "xml"
+                 :destination "serialized"}]
+    (-> (xf (clj->js options))
+        (. -principalResult))))
 
 (try
   (let [name-to-greet (.getInput core "who-to-greet")
@@ -9,6 +35,7 @@
         _ (.setOutput core "time" time)
         payload (.. github -context -payload)
         payload (js/JSON.stringify payload nil 2)
-        _ (.log js/console (str "The event payload: " payload))])
+        _ (.log js/console (str "The event payload: " payload))]
+      (.log js/console (str (transform (node-slurp "source.xml")))))
   (catch :default e
     (.setFailed core (.-message e))))
---
+++ source.xslt
@@ -0,0 +1,8 @@
+<xsl:stylesheet version="3.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    <xsl:output method="xml" indent="yes"/>
+    <xsl:template match="@* | node()">
+        <xsl:copy>
+            <xsl:apply-templates select="@* | node()"/>
+        </xsl:copy>
+    </xsl:template>
+</xsl:stylesheet>
